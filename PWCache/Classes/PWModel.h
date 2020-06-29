b262cf9bff521aef1f0e9b64ccdf91292311e86a
Lpw <727626565@qq.com>
2020-06-29 21:05:04 +0800

    one New Project

---
+++ PWCache/Classes/PWModel.h
@@ -0,0 +1,17 @@
+//
+//  PWModel.h
+//  FBSnapshotTestCase
+//
+//  Created by 小siri on 2020/6/29.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PWModel : NSObject
+/**   **/
+@property (nonatomic, strong) NSString *name;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PWCache/Classes/PWModel.m
@@ -0,0 +1,12 @@
+//
+//  PWModel.m
+//  FBSnapshotTestCase
+//
+//  Created by 小siri on 2020/6/29.
+//
+
+#import "PWModel.h"
+
+@implementation PWModel
+
+@end
